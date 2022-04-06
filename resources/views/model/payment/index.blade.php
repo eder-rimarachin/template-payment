70b504af3eb8d561f331991d67de96dfb0fb5f0b
Joaquin <Ederrr2802@gmail.com>
2022-04-06 14:52:34 -0500

    finish template payment

--- resources/views/model/payment/index.blade.php
+++ resources/views/model/payment/index.blade.php
@@ -19,7 +19,6 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
 
     <style>
         .payment-form {
-            padding-bottom: 50px;
             font-family: 'Montserrat', sans-serif;
         }
 
@@ -40,7 +39,7 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
 
         .payment-form .block-heading p {
             text-align: center;
-            max-width: 420px;
+            max-width: 720px;
             margin: auto;
             opacity: 0.7;
         }
@@ -62,7 +61,7 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
             background-color: #ffffff;
             padding: 0;
             min-width: 200px;
-            max-width: 800px;
+            max-width: 1200px;
             margin: 0 auto;
         }
 
@@ -115,7 +114,7 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
         .payment-form .card-details {
 
             padding: 25px 25px 15px;
-            box-shadow: 0px 0px 3px 1px #79818a;
+            /* box-shadow: 0px 0px 3px 1px #79818a; */
         }
 
         .payment-form .card-details label {
@@ -138,19 +137,6 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
             margin-top: 5px;
         }
 
-        .p-right {
-            vertical-align: bottom;
-            min-width: 350px;
-            float: right;
-            shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
-        }
-
-        .p-left {
-            min-width: 350px;
-            float: left;
-            shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
-        }
-
         .u-learn {
             font-size: 36px;
             margin: 0 auto;
@@ -159,27 +145,93 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
             color: #3b99e0;
         }
 
+        @media only screen and (min-width: 1200px) {
+            .container {
+                width: 100vw;
+                margin: 0 auto;
+                padding: 0;
+            }
+
+            .tarjet-form {
+                width: 100%;
+                margin: 10px;
+                padding: 0;
+                display: flex;
+                justify-content: space-around;
+            }
+
+            .p-right {
+                order: 2;
+                vertical-align: bottom;
+                max-width: 800px;
+                min-width: 350px;
+                float: right;
+
+            }
+
+            .p-left {
+                order: 1;
+                min-width: 350px;
+                width: 600px;
+                float: left;
+            }
+        }
+
+        @media only screen and (max-width: 1199px) {
+            .p-right {
+                vertical-align: bottom;
+                width: 300px;
+                min-width: 350px;
+                float: right;
+                shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
+            }
+
+            .p-left {
+                min-width: 350px;
+                width: 450px;
+                float: left;
+                shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
+            }
+        }
+
+        @media only screen and (max-width: 992px) {
+            .tarjet-form {
+                width: 500px;
+                display: flex;
+                flex-direction: column-reverse;
+            }
+
+            .p-right {
+                width: 400px;
+                margin: 0 auto;
+            }
+
+            .p-left {
+                width: 400px;
+                margin: 10px auto;
+                padding: 10px auto;
+            }
+
+        }
+
         @media only screen and (max-width: 768px) {
 
             .p-right {
                 margin: 0 auto;
                 padding: 0 auto;
-                min-width: 300px;
+                min-width: 250px;
                 width: 350px;
 
             }
 
             .p-left {
-                min-width: 300px;
+                min-width: 250px;
                 width: 350px;
-                margin: 0 auto;
-                padding: 0 auto;
+                margin: 10px auto;
+                padding: 10px auto;
             }
 
             .tarjet-form {
-
-                margin: 0 auto;
-                padding: 0 auto;
                 display: flex;
                 flex-direction: column-reverse;
             }
@@ -188,13 +240,20 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
 
         @media only screen and (max-width: 480px) {
             .container {
-                height: 100vh;
+                width: 100vw;
+                height: 110vh;
+                margin: 0 auto;
+                padding: 0;
+            }
+
+            .tarjet-form {
+                margin: 0;
+                padding: 0;
+                width: 80vw;
             }
 
             .p-right {
                 width: 80vw;
-                margin: 10px auto;
-                padding: 10px auto;
             }
 
             .p-left {
@@ -204,125 +263,123 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
             }
 
             .btn-pay {
-                padding: 0;
-                margin: 0;
-                width: 100%;
-                height: 100px;
+                width: 100vw;
+                height: 80px;
                 position: fixed;
                 bottom: 0;
                 z-index: 1030;
                 left: 0;
                 background-color: #f7fbff;
                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.452);
-
             }
 
             .btn-btp {
-                margin-top: 20px;
-                margin-left: 170px;
-                padding: 0 auto;
                 width: 200px;
-                height: 50px;
+                margin-left: 45%;
+                margin-bottom: 20px;
+                position: fixed;
+                bottom: 0;
+                z-index: 1030;
+                left: 0;
             }
 
             .total {
-                margin-bottom: 25px;
-                margin-left: 45px;
-                font-size: 28px;
+                font-size: 1.5em;
                 display: flex;
-                flex-direction: column;
                 position: fixed;
                 bottom: 0;
                 z-index: 1050;
-                left: 0;
-                color: #000;
+                flex-direction: column;
+                border-top: none !important;
+                background-color: none !important;
+                margin-bottom: 3%;
             }
 
-            .total-to{
-                font-weight: normal;
-                font-size: 18px;
+            .total-to {
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                font-size: 1em;
+                font-weight: normal !important;
             }
 
-            @media only screen and (max-width: 375px) {
-                .container {
-                    height: 130vh;
-                }
+            .price {
+                font-size: 1.2em;
+                font-weight: 600;
+            }
+        }
 
-                .btn-btp {
-                    margin-top: 20px;
-                    margin-left: 150px;
-                    padding: 0 auto;
-                    width: 200px;
-                    height: 50px;
-                }
-
-                .total {
-                    margin-bottom: 25px;
-                    margin-left: 45px;
-                    font-size: 28px;
-                    display: flex;
-                    flex-direction: column;
-                    position: fixed;
-                    bottom: 0;
-                    z-index: 1050;
-                    left: 0;
-                }
+        @media only screen and (max-width: 375px) {
+            .container {
+                width: 100vw;
+                height: 110vh;
+                margin: 0 auto;
+                padding: 0;
             }
 
-            @media only screen and (max-width: 280px) {
-                .p-right {
-                    margin: 0 auto;
-                    padding: 0 auto;
-                    min-width: 90vw;
+            .btn-btp {
+                width: 50%;
+            }
 
-                }
+            .tarjet-form {
+                margin: 0;
+                padding: 0;
+                width: 90vw;
 
-                .p-left {
-                    min-width: 90vw;
-                    margin: 0 auto;
-                    padding: 0 auto;
-                }
-
-                .tarjet-form {
-                    width: 90vw;
-                    margin: 0 auto;
-                    padding: 0 auto;
-                    display: flex;
-                    flex-direction: column-reverse;
-                }
-
-                .btn-pay {
-                    height: 60px;
-                    position: fixed;
-                    bottom: 0;
-                    z-index: 1030;
-                    left: 0;
-                    background-color: #000;
-
-                }
-
-                .btn-btp {
-
-                    margin-left: 100px;
-                    padding: 0 auto;
-                    height: 40px;
-                    width: 150px;
-                    margin-top: 10px;
-                }
-
-                .total {
-                    margin-bottom: 15px;
-                    margin-left: 35px;
-                    font-size: 16px;
-                    display: flex;
-                    flex-direction: column;
-                    position: fixed;
-                    bottom: 0;
-                    z-index: 1050;
-                    left: 0;
-                }
 
             }
+
+            .p-right {
+                width: 80vw;
+            }
+
+            .p-left {
+                width: 80vw;
+                margin: 10px auto;
+                padding: 10px auto;
+            }
+        }
+
+        @media only screen and (max-width: 280px) {
+            .container {
+                width: 100vw;
+                height: 125vh;
+                margin: 0 auto;
+                padding: 0;
+            }
+
+            .btn-btp {
+                width: 50%;
+            }
+
+            .tarjet-form {
+                margin: 0;
+                padding: 0;
+                width: 90vw;
+
+            }
+
+            .p-right {
+                width: 80vw;
+            }
+
+            .p-left {
+                width: 80vw;
+                margin: 10px auto;
+                padding: 10px auto;
+            }
+
+            .btn-pay {
+                height: 70px;
+            }
+
+            .btn-btp {
+                margin-bottom: 15px;
+            }
+
+            .total {
+                font-size: 1.2em;
+                margin-bottom: 15px;
+            }
+        }
 
     </style>
 </head>
@@ -347,7 +404,8 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
                             <p class="item-name">Product 2</p>
                             <p class="item-description">Lorem ipsum dolor sit amet</p>
                         </div>
-                        <div class="total"><span class="total-to">Total</span><span class="price">$320</span></div>
+                        <div class="total"><span class="total-to">Total</span><span
+                                class="price">$320</span></div>
                     </div>
                     <div class="card-details p-left">
                         <h3 class="title">Pagar</h3>
@@ -377,7 +435,7 @@ if ($url == 'http://localhost:8000/' || $url == 'http://127.0.0.1:8000/') {
                                 <input id="cvc" type="text" class="form-control" placeholder="CVC"
                                     aria-label="Card Holder" aria-describedby="basic-addon1">
                             </div>
-                            <div class="form-group col-sm-12 btn-pay">
+                            <div class="form-group col-sm-12">
                                 <div class="btn-pay">
                                     <button type="button" class="btn btn-primary btn-block btn-btp">Realizar
                                         Pago</button>
